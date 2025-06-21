0f6845948603cf339e99d41ef5a39fa403f1f072
Lucas G. Bueno <lucasgbueno@proton.me>
2025-06-21 15:25:10 -0300

    config hypridle and lock screen

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -6,11 +6,9 @@ if [[ $XDG_SESSION_TYPE == 'x11' ]]; then
 
   xset s 500 &
   xautolock -time 5 -locker "betterlockscreen -l" \
-    -notify 30 -notifier "dunstify -i ~/.config/eww/images/lock.svg -a 'Locker' 'Locker  ' 'Locking screen in 30 seconds'" \
+    -notify 30 -notifier "dunstify -i ~/.config/eww/images/lock.svg -a 'Locker' 'Locker' 'Locking screen in 30 seconds'" \
     -killtime 10 -killer "systemctl suspend" &
 
 elif [[ $XDG_SESSION_TYPE == 'wayland' ]]; then
-  swayidle -w \
-    timeout 570 'dunstify -i ~/.config/eww/images/lock.svg -a "Locker" "Locker  " "Locking screen in 30 seconds"' \
-    timeout 600 "hyprlock "
+  hypridle
 fi
